860869c62444df5a967057ee93b46b180110a7cd
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2023-10-12 09:34:05 -0300

    master auto-commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# music-generator
+# expense-calculator
 ReadMe: https://docs.google.com/document/d/19lkT65C9NcD5OsXxkafJqbmzen521sgUvCQ6hhS4HbM/edit
--- music-generator.py
+++ music-generator.py
@@ -1,12 +1,32 @@
 import tkinter as tk
-import playsound as pSound
 
-def reproducir_sonido():
-    pSound("sonido.wav")
+# Función para abrir la pantalla de registro de gastos
+def abrir_registro():
+    ventana_principal.destroy()  # Cerrar la ventana principal
+    # Aquí implementarás la pantalla de registro de gastos
 
-root = tk.Tk()
+# Función para abrir la pantalla de resumen de gastos
+def abrir_resumen():
+    ventana_principal.destroy()  # Cerrar la ventana principal
+    # Aquí implementarás la pantalla de resumen de gastos
 
-boton_reproducir = tk.Button(root, text="Reproducir", command=reproducir_sonido)
-boton_reproducir.pack()
+# Configuración de la ventana principal
+ventana_principal = tk.Tk()
+ventana_principal.title("Calculadora de Gastos")
+ventana_principal.geometry("500x400")
+ventana_principal.configure(bg="orange")
 
-root.mainloop()
+# Crear etiqueta de bienvenida
+etiqueta_bienvenida = tk.Label(ventana_principal, text="Bienvenido a la Calculadora de Gastos", width=400, height=3, bg="red", font=("Segoe Script", 15))
+etiqueta_bienvenida.pack(pady=20)
+
+#Crear botones para registrar gastos y ver resumen
+boton_registrar_gasto = tk.Button(ventana_principal, text="Registrar Gasto", command=abrir_registro, width=20, height=3)
+boton_ver_resumen = tk.Button(ventana_principal, text="Ver Resumen", command=abrir_resumen, width=20, height=3)
+
+# Colocar los botones en la ventana principal
+boton_registrar_gasto.pack(pady=30)
+boton_ver_resumen.pack()
+
+# Iniciar la aplicación
+ventana_principal.mainloop()
