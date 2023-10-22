ed87d15b8913afeb48ffdad75a0f37f30d856ef9
Franco Reyes <d3ksur@Francos-MacBook-Pro.local>
2023-10-22 20:35:34 -0300

    master auto-commit

--- expense-calculator.py
+++ expense-calculator.py
@@ -3,19 +3,33 @@ from tkinter import messagebox
 import matplotlib.pyplot as plt
 
 gastos = {}
+opciones_tipos = ["Alimentación", "Transporte", "Entretenimiento", "Servicios", "Otros"]
 
 # Crear la ventana de registro de gasto
 def crear_ventana_registro():
     ventana_registro = tk.Toplevel(ventana_principal)
     ventana_registro.title("Registrar Nuevo Gasto")
-    ventana_registro.geometry("300x250")
+    ventana_registro.geometry("400x500")
     ventana_registro.configure(bg="black")
+    
+    ventana_registro_label = tk.Label(ventana_registro, text="Registrar gasto", width=400, height=3, bg="gray", font=("Segoe Script", 15), relief=tk.RAISED)
+    ventana_registro_label.pack(pady=20)
 
     # Elementos de la interfaz de usuario para el registro de gastos
     nombre_label = tk.Label(ventana_registro, text="Nombre del gasto:", bg="gray", font=("Segoe Script", 10), relief=tk.RAISED)
     nombre_label.pack(pady=10)
     nombre_entry = tk.Entry(ventana_registro)
     nombre_entry.pack(pady=5)
+    
+    tipo_label = tk.Label(ventana_registro, text="Tipo de gasto:", bg="gray", font=("Segoe Script", 10), relief=tk.RAISED)
+    tipo_label.pack(pady=10)
+
+    # Opciones de clasificación de gastos
+    opciones_tipos = ["Alimentación", "Transporte", "Entretenimiento", "Servicios", "Otros"]
+    tipo_variable = tk.StringVar(ventana_registro)
+    tipo_variable.set(opciones_tipos[0])  # Valor predeterminado
+    tipo_menu = tk.OptionMenu(ventana_registro, tipo_variable, *opciones_tipos)
+    tipo_menu.pack(pady=5)
 
     def validate_input(new_text):
         if not new_text:
@@ -46,13 +60,15 @@ def crear_ventana_registro():
 
         nombre = nombre_entry.get()
         monto = float(monto_entry.get())
+        tipo = tipo_variable.get()
 
         if nombre in gastos:
-            gastos[nombre].append(monto)
+            gastos[nombre].append((tipo, monto))
         else:
-            gastos[nombre] = [monto]
+            gastos[nombre] = [(tipo, monto)]
 
-        messagebox.showinfo("Gasto registrado", f"Se registró un gasto de {monto} en {nombre}")
+        messagebox.showinfo("Gasto registrado", f"Se registró un gasto de {monto} en {nombre} de tipo {tipo}.")
+
 
     registrar_button = tk.Button(ventana_registro, text="Registrar", command=registrar_gasto)
     registrar_button.pack(pady=20)
@@ -72,17 +88,30 @@ def crear_ventana_registro():
 
 #Funcion para crear el grafico con los datos guardados.
 def update_graph():
-    nombres = list(gastos.keys())
-    montos = [sum(gastos[nombre]) for nombre in nombres]
+    fig, axs = plt.subplots(1, len(opciones_tipos), figsize=(6 * len(opciones_tipos), 4), squeeze=False)
 
-    plt.figure(figsize=(6, 4))
-    plt.bar(nombres, montos, color='skyblue')
-    plt.xlabel('Gastos')
-    plt.ylabel('Monto total')
-    plt.title('Gastos Registrados')
-    plt.xticks(rotation=45)
+    for i, tipo in enumerate(opciones_tipos):
+        tipo_gastos = {}
+        for nombre, detalles in gastos.items():
+            for t, m in detalles:
+                if t == tipo:
+                    if nombre in tipo_gastos:
+                        tipo_gastos[nombre] += m
+                    else:
+                        tipo_gastos[nombre] = m
+
+        nombres = list(tipo_gastos.keys())
+        montos = list(tipo_gastos.values())
+
+        axs[0, i].bar(nombres, montos, color='skyblue')
+        axs[0, i].set_xlabel('Nombres de Gastos')
+        axs[0, i].set_ylabel('Monto total')
+        axs[0, i].set_title(f'Gastos de tipo {tipo}')
+        axs[0, i].tick_params(axis='x', rotation=45)
+
     plt.tight_layout()
     plt.show()
+
 
 # Función para abrir la pantalla de resumen de gastos
 def abrir_resumen():
@@ -93,7 +122,7 @@ ventana_principal = tk.Tk()
 ventana_principal.title("Calculadora de Gastos")
 ventana_principal.geometry("500x400")
 ventana_principal.configure(bg="black")
-ventana_principal.iconbitmap('icons8-calculator-16.ico')
+# ventana_principal.iconbitmap('icons8-calculator-16.ico')
 
 etiqueta_bienvenida = tk.Label(ventana_principal, text="Bienvenido a la Calculadora de Gastos", width=400, height=3, bg="gray", font=("Segoe Script", 15), relief=tk.RAISED)
 etiqueta_bienvenida.pack(pady=20)
